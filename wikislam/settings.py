8bd7599b6304764b87d1593570c06714df0504f8
unknown <benyamine@outlook.es>
2020-09-01 06:28:48 +0200

    a

--- wikislam/settings.py
+++ wikislam/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'wikislam.wsgi.application'
 
 
 
-""" 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +101,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+""" 
 """ import dj_database_url
 
 from decouple import config
@@ -176,4 +176,3 @@ EMAIL_HOST_PASSWORD = 'fibonacsci'
 EMAIL_PORT = 587
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
